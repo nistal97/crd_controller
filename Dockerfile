58a1699f4426ac3b761c6d5ea93cc04ea13abdc1
ydong <ydong@ebay.com>
2021-10-19 14:54:15 +0800

    init

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM hub.tess.io/sherlockio/debian:stretch
+
+RUN mkdir -p /platform
+
+COPY output/crd_controller /bin/crd_controller
+
+WORKDIR /platform
+
+ENTRYPOINT [ "/bin/crd_controller" ]
+CMD []
--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
-export GOROOT=E:/dev/tools/go1.17.windows-amd64/go
-${GOROOT}/bin/go mod tidy
-${GOROOT}/bin/go mod vendor
-${GOROOT}/bin/go build -v -o ./output/crd_controller ./cmd/crd_controller
+go mod tidy
+go mod vendor
+go build -v -o ./output/crd_controller ./cmd/crd_controller
